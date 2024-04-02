808efd19f903222cedd84490ac694d846a62b29b
Stepanova Diana <stepanova.diana26092002@gmail.com>
2024-04-03 02:17:33 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+/*import {useEffect} from "react";
+import {useT} from "./hooks/useT";
+import Header from "./component/header/header";
+import {Route, Routes} from "react-router-dom";
+import TestList from "./testList/testList";
+import Form from "./form/form";
+import MeningocalInfection from "./component/meningocalInfection/meningocalInfection";
+
+*/
 function App() {
+  //const {tg} = useT();
+
+ /* useEffect( () => {
+    tg.ready();
+  },  [])*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        frf
+      </div>
   );
 }
 
 export default App;
+/* <Header />
+        <Routes>
+          <Route index element = {<TestList />}/>
+          <Route path={'form'} element = {<Form />}/>
+          <Route path={'test1'} element = {<MeningocalInfection />}/>
+
+        </Routes>*/
